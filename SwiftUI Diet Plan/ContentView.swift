631121040d7047ce0b8245a80da1e79465a39ce6
Prashant3-playo <prashant@playo.co>
2024-12-19 19:06:20 +0530

    first commit

---
+++ SwiftUI Diet Plan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Diet Plan
+//
+//  Created by Prashant Kumar Soni on 19/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Diet Plan/SwiftUI_Diet_PlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Diet_PlanApp.swift
+//  SwiftUI Diet Plan
+//
+//  Created by Prashant Kumar Soni on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Diet_PlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
